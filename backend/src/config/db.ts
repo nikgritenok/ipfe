673efa9ed253fe1ffba5849d6eb2c10a7758ca31
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-02-10 16:42:13 +0700

    feat(backend): изменил подключение к бд на контейнер docker

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mydb')
+    await mongoose.connect(
+      'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000',
+    )
     console.log('MongoDB подключен!')
   } catch (error) {
     console.error('Ошибка подключения к MongoDB', error)
